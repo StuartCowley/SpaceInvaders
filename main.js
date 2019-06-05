9ee0e99f807faf641eaaf8020d32a886aa708964
StuartCowley <codenation18@CodeNation18s-MacBook-Pro.local>
2019-06-05 14:55:24 +0100

    added square and rectangle to JS

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 const background = document.getElementById("gameBoard");
-var ctx = c.getContext("2d");
-ctx.moveTo(0,0);
-ctx.lineTo(200,100);
-ctx.stroke();
+var ctx = background.getContext("2d");
+
+ctx.fillStyle = 'white';
+ctx.fillRect(20, 20, 80, 80);
+
+ctx.fillStyle = 'green';
+ctx.beginPath();
+ctx.moveTo(20, 600);
+ctx.lineTo(100, 600);
+ctx.lineTo(80, 540);
+ctx.lineTo(40, 540);
+ctx.fill();
